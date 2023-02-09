0f326aa8a6edabf454db5bdac1f8583c02457beb
Owolabi Pius <opius2007@gmail.com>
2023-02-09 04:49:40 +0100

    String update

--- string.c
+++ string.c
@@ -103,17 +103,11 @@ int _strlen(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+	int i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
 }
 
 /**
